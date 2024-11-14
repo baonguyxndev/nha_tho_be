adebe5883962a4e0c237357c507fd089b6a3a448
yonB280405 <giabao2005bbb@gmail.com>
2024-11-14 11:05:46 +0700

    Add: login(generate  access token jwt)

--- src/auth/module/auth.module.ts
+++ src/auth/module/auth.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthController } from '../controller/auth.controller';
+import { AuthService } from '../service/auth.service';
+import { AdminModule } from '@/modules/admin/module/admin.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    AdminModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_ACCESS_TOKEN_EXPIRED'),
+        },
+      }),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,26 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+
+import { comparePasswordHelper } from '@/helpers/util';
+import { AdminService } from '@/modules/admin/service/admin.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { access } from 'fs';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    private adminService: AdminService,
+    private jwtService: JwtService,
+  ) { }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async login(email: string, password: string): Promise<any> {
+    const admin = await this.adminService.findByEmail(email);
+    const isValidPassword = comparePasswordHelper(password, admin.password);
+    if (!isValidPassword)
+      throw new UnauthorizedException("Email/Password không hợp lệ");
+    const payload = { sub: admin._id, email: admin.email };
+    return {
+      accessToken: await this.jwtService.signAsync(payload)
+    }
   }
 }
